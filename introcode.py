6e53725dac32d45b3a394c1a8cef3c7b68fb1c4b
cherrywjones <cherry.jones@cefas.co.uk>
2019-09-13 11:00:52 +0100

    Added for loop to id if number is less than 5

--- introcode.py
+++ introcode.py
@@ -2,3 +2,9 @@
 #this is a text file for the introduction session of the github course
 import numpy as np
 print('hello world')
+for number in range(10):
+    if number * 2 < 10:
+        print(str(number) + ' is less that 5')
+    
+
+    
